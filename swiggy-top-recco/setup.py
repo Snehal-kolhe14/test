723407ff3526c2921dcee587ed098075bda6b3df
Snehal-kolhe14 <snehal.kolhe14@gmail.com>
2023-12-04 21:17:06 +0530

    cookicutter initial commit

---
+++ swiggy-top-recco/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will reccomend top food items',
+    author='Snehal Kolhe',
+    license='',
+)
